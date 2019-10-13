4761e38db121ca50834f035b6be870696d2681ca
Harsh Asnani <harshmasnani@gmail.com>
2019-10-13 19:37:54 -0400

    adding verifyTimestamps.py
    small pyscript to make sure timestamps is consolidated raw data are ordered. Faced some issues because of overlapping timestamps in 2 files for consequent dates. Manually deleted rows from consolidated file for now. Will write a join script that deleted overlapping elements. Created consolidated file from merge-csv.com

---
+++ Code/verifyTimestamps.py
@@ -0,0 +1,15 @@
+##VERIFYING timestamps in consolidated file are in order
+import pandas as pd
+
+df=pd.read_csv('/Users/Sunny/RA/StopClassification/Data/raw/raw-concatenated/C1415.csv')
+df.Index=df.index
+
+for i in range(1,len(df.index)):
+    if (df.at[i-1,'Date and Time'] > df.at[i,'Date and Time']):
+        print('Time stamping is unordered. Check ' + str(i-1) + ',' + str(i))
+        break
+    if(i==len(df.index)-1):
+        print('File is rightly ordered')
+
+##Write this in console when File is rightly ordered.
+#df.to_csv("/Users/Sunny/RA/StopClassification/Data/raw/raw-concatenated/C1415.csv", index=False)
